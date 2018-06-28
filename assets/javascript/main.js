3c1ff800997c06b16d90cf24924ce26c2db2ebc0
liset valdes <lvaldes@techlaunch.io>
2018-06-28 10:51:51 -0400

    added other pages

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,18 +1,25 @@
-window.onload = function(){
-    document.getElementById('hamber').addEventListener('click', function(){
-        var classApplied = document.getElementById('navbar').className;
-        if (classApplied === 'visible'){
-            document.getElementById('navbar').className = 'hidden';
-        }else{
-            document.getElementById('navbar').className = 'visible';
+window.onload = function () {
+    document.getElementById('hamber').addEventListener('click', function () {
+    var classApplied = document.getElementById('navbar').className;
+    if (classApplied === 'visible') {
+        document.getElementById('navbar').className = 'hidden';
+    } else {
+        document.getElementById('navbar').className = 'visible';
         }
 
     })
+    var patharray = location.pathname.split('/');
+    var folderName = patharray[1];
+    if (folderName == '' || folderName == 'index.html') {
+            document.getElementById('home').className = 'currentpage';
+    } else {
+        var navbar = document.getElementById('navbar');
+        var links = navbar.getElementsByTagName('a');
+        for (i = 0; i < links.length; i++) {
+            if (links[i].getAttribute('href').indexOf(folderName) > -1) {
+                links[i].className = 'currentpage';
+            }
+        }
+    }
+
 }
-
-
-//ternary operator
-
-//someting = if true ? then : else
-// ? if true the value after
-// : else the value after
